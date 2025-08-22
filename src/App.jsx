ef6567ff64f83ce736d864fab63c3eaaf891c933
Volker Friedetzki <info@friedesoft.de>
2025-08-22 12:27:48 +0200

    Pages different files

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
-function Home() {
-  const [count, setCount] = useState(0);
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
-}
-
-function Page2() {
-  return <div><h2>Page2</h2></div>;
-}
+import Home from './Home';
+import Page2 from './Page2';
 
 function App() {
   return (
@@ -44,8 +14,8 @@ function App() {
         <Link to="/Page2">Page2</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Page2" element={<Page2 />} />
+  <Route path="/" element={<Home />} />
+  <Route path="/Page2" element={<Page2 />} />
       </Routes>
     </Router>
   );
